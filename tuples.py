e326fa26d1e52634115df3bff981b288e23bfb9a
Ryan Ottney <rottney123@gmail.com>
2019-09-19 22:58:16 -0700

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,16 @@
+"""
+Task
+Given an integer, n, and n space-separated integers as input, create a tuple, t, of those n integers.
+Then compute and print the result of hash(t).
+Input Format
+The first line contains an integer, n, denoting the number of elements in the tuple. 
+The second line contains n space-separated integers describing the elements in tuple t.
+Output Format
+Print the result of hash(t).
+"""
+
+if __name__ == '__main__':
+    n = int(input())    # this came by default, idk...
+    integer_list = map(int, input().split())
+    t = tuple(integer_list)
+    print(hash(t))
